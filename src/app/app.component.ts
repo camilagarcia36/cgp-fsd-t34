c160bbae520900f7756c6fbda96e4c58d4dcdfb3
camilagarcia36 <garciiaperezz@gmail.com>
2023-05-15 11:40:51 +0200

    calculadora acabada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 't34';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  operador = '0';
+  resultado = '0';
+
+  addNum(num: string)
+  {
+    if(this.resultado != '0' && num != '00')
+    {
+      this.operador = num;
+    }
+    else if(((this.operador == '0' || this.operador == '') || this.resultado != '0') && num == '00')
+    {
+      this.operador = '0';
+    }
+    else if(this.operador == '0')
+    {
+      this.operador = num;
+    }
+    else
+    {
+      this.operador += num;
+    }
+    this.resultado = '0';
+  }
+
+  addOperador(op: string)
+  {
+    if(this.resultado != '0')
+    {
+      this.operador = this.resultado + op;
+    }
+    else
+    {
+      this.operador += op;
+    }
+    this.resultado = '0';
+  }
+
+  delete()
+  {
+    this.operador = this.operador.substring(0,this.operador.length-1);
+  }
+
+  evaluar()
+  {
+    this.resultado = eval(this.operador);
+  }
 }
